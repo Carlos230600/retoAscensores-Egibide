b71cbed591c8ab70bf9f71c362a5840c2e237646
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-08 00:28:02 +0100

    Update manuales.blade.php

--- resources/views/manuales.blade.php
+++ resources/views/manuales.blade.php
@@ -187,11 +187,6 @@
 
 </style>
 </head>
-<script src="./sweetalert2.all.min.js"></script>
-<script src="./jquery-3.6.0.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<sctipt src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></sctipt>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <body>
     @include('header')
 
