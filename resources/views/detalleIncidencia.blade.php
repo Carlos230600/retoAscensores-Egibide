c0a5cbf2af5b8edcb4d2d537d870c0eacf8085ba
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-07 18:20:50 +0100

    Update detalleIncidencia.blade.php

--- resources/views/detalleIncidencia.blade.php
+++ resources/views/detalleIncidencia.blade.php
@@ -267,15 +267,8 @@
                             </div>
                         </li>
                     </ul>
-                        
-                    <div id="addComent" class="manOpc">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-text" viewBox="0 0 16 16">
-                            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-                            <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
-                        Añadir comentario
-                    </div>
-                    <div id="comentario" style="display: none; height: auto;">
+                       
+                    <div id="comentario" height: auto;">
                         <textarea class="form-control" maxlength="150" rows="2" placeholder="Escribe aqui tu comentario"></textarea>
                         <!--Añadir comentario-->
                         <div id="enviar">
@@ -289,11 +282,7 @@
         </div>
     </div>
 
-     <script>
-        $("#addComent").click(function(){
-            $("#comentario").slideToggle("slow");
-        })
-        
+     <script>       
         //Confirma estado de incidencia antes de salir
         function navegar(){
             return new Promise((resolve,reject)=>{
@@ -391,9 +380,9 @@
             listaComentarios.appendChild(comentario);
         });
 
-        //Muestra los ultimos comentarios (scrollbar en el bottom)
+        /*Muestra los ultimos comentarios (scrollbar en el bottom)
         var objDiv = document.getElementById("manOpc4");
-        objDiv.scrollTop = objDiv.scrollHeight;
+        objDiv.scrollTop = objDiv.scrollHeight;*/
     </script>
     
 </body>
