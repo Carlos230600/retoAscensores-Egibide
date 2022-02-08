2c85b35181151342f3d3bc32be20420139b12e98
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-08 12:53:09 +0100

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -185,11 +185,6 @@
             }
         }
     </style>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.3.10/dist/sweetalert2.all.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         
        function navegar(){
